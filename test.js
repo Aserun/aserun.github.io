e3fa4d1231e1ca9479e2ddcded07e3fce7544d78
Mars <31801886+Aserun@users.noreply.github.com>
2019-05-06 18:24:05 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-var m = document.querySelector("a-marker");
-m.addEventListener("markerFound", (e)=>{console.log("found")})
-m.addEventListener("markerLost", (e)=>{console.log("lost")})
+document.addEventListener("DOMContentLoaded", function(){
+    var m = document.querySelector("a-marker");
+    m.addEventListener("markerFound", (e)=>{console.log("found")});
+    m.addEventListener("markerLost", (e)=>{console.log("lost")});
+}
